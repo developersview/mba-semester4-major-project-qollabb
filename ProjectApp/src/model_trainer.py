c319904cbe98e203eba83ec1ecc43f07a6336fde
developersview <pcslg1998@gmail.com>
2025-05-09 09:06:38 +0530

    updated project report - working on machine learning part

--- ProjectApp/src/model_trainer.py
+++ ProjectApp/src/model_trainer.py
@@ -30,7 +30,6 @@ X_vec = vectorizer.fit_transform(X)
 # Train-test split
 X_train, X_test, y_train, y_test = train_test_split(X_vec, y, test_size=0.2, random_state=42)
 
-
 # Train Logistic Regression
 log_reg = LogisticRegression(class_weight='balanced')
 log_reg.fit(X_train, y_train)
